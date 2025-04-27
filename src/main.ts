2c837ae6651f91963e382d7fbad5433090c33e73
Ridie-Kings <fran6salvatierra@gmail.com>
2025-04-27 13:36:21 +0200

    feat: add express middleware for JSON and URL-encoded request handling with increased limits

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as express from 'express';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(express.json({ limit: '20mb' }));
+  app.use(express.urlencoded({ limit: '20mb', extended: true }));
+
   await app.listen(process.env.PORT ?? 4327);
 }
 bootstrap();
