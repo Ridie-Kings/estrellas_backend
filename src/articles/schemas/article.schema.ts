b82b36324fa47dfce84aa8589fcda95745245962
Ridie-Kings <fran6salvatierra@gmail.com>
2025-04-28 16:21:44 +0200

    fix: change categories field type in ArticleSchema from String to array of Strings

--- src/articles/schemas/article.schema.ts
+++ src/articles/schemas/article.schema.ts
@@ -4,7 +4,7 @@ export const ArticleSchema = new Schema({
   title: String,
   slug: String,
   content: String,
-  categories: String,
+  categories: [String],
   date: { type: Date, default: Date.now },
 });
 
